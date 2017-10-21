d722a06dc2335f910d5ecc8de876212d0a12600d
Spri85 <viktorpar@gmail.com>
2017-10-21 17:22:23 +0300

    find Insert into Collectio

--- client/main.js
+++ client/main.js
@@ -12,6 +12,24 @@ Template.main.helpers({
     return 'QuickTodos';
   },
   todos() {
-    return tasks;
+    const todos = Todos.find();
+    return todos;
+  }
+});
+
+Template.main.events({
+  'submit .add-todo'(event) {
+    event.preventDefault();
+
+    const text = event.target.text.value;
+    const time = event.target.time.value;
+
+    Todos.insert({
+      text,
+      time
+    });
+
+    event.target.text.value = '';
+    event.target.time.value = '';
   }
 });
